3b5733a53d1d4b401cbf561a14649e36f4cfd232
Paul Banach <pawel.banach@chargify.com>
2021-03-20 22:58:21 +0100

    Enable frozen_string_literal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { errors: [{ id: params[:id], detail: exception.message }] }, status: :not_found
