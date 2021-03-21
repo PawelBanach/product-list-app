46e763ebee37061165da975f9955f3c3d48905a5
Paul Banach <pawel.banach@chargify.com>
2021-03-21 16:18:13 +0100

    Handle StandardError and ParameterMissing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { errors: [{ id: params[:id], detail: exception.message }] }, status: :not_found
+  end
+
+  rescue_from ActionController::ParameterMissing, StandardError do |exception|
+    render json: { errors: [{ detail: exception.message }] }, status: :unprocessable_entity
   end
 end
