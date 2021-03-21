2b372ffb7a16ba2a23d0e5c73a602d2917db1694
Paul Banach <pawel.banach@chargify.com>
2021-03-21 15:40:43 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+tags = Tag.create(
+  [
+    { title: "Sparkle" },
+    { title: "Still" },
+  ]
+)
+
+Product.create(
+  [
+    { name: "Water", description:"24oz Bottle", price: 1.98, tags: tags },
+    { name: "Wine", description:"24oz Bottle", price: 1.48, tags: tags },
+  ]
+)
+
